20adc2d9b29ef677cedc42ba64799374ab1cb557
Wang. Hanli <h.wang@seeburger.de>
2021-02-24 16:06:07 +0800

    Create a simple global basic configuration file for general purpose, such as GC, Avy, etc;

---
+++ global-basic.el
@@ -0,0 +1,7 @@
+;; some basic configuration - global
+
+(setq ring-bell-function 'ignore)
+
+(delete-selection-mode 1)
+
+(put 'dired-find-alternate-file 'disabled nil)
--- init.el
+++ init.el
@@ -34,3 +34,6 @@
 
 (setq language-file "~/.emacs.d/lang/general.el")
 (load-file language-file)
+
+(setq global-basic-config-file "~/.emacs.d/global-basic.el")
+(load-file global-basic-config-file)
