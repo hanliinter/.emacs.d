5b5e13663f8850e353d79d53cf90bb6f67c30398
Wang. Hanli <h.wang@seeburger.de>
2021-03-01 14:31:20 +0800

    Finish tweaking ace-window

--- personal-behaviour.el
+++ personal-behaviour.el
@@ -2,8 +2,8 @@
 
 (use-package multiple-cursors
   :straight t
-  :bind (("C-c m m" . #'mc/edit-lines )
-         ("C-c m d" . #'mc/mark-all-dwim )))
+  :bind (("C-c e e" . #'mc/edit-lines )
+         ("C-c e d" . #'mc/mark-all-dwim )))
 
 (use-package expand-region
   :straight t
@@ -12,6 +12,24 @@
 (bind-key "C-c /" #'comment-dwim)
 
 
+(use-package duplicate-thing
+  :straight t
+  :init
+  (defun duplicate-current-line ()
+    "duplicate the current line but save the mark, inspired by https://blog.sumtypeofway.com/posts/emacs-config.html"
+    (interactive)
+    (save-mark-and-excursion (duplicate-thing 1)))
+  :bind (("C-c d" . duplicate-current-line)))
+
+
+;; Jumping
+
 (use-package ace-window
   :straight t
+  :config
+  (ace-window-display-mode)
+  :custom
+  (aw-keys '(?a ?s ?d ?f ?q ?w ?e ?r))
   :bind (("C-x o" . ace-window))) 
+
+;; Switch to some default buffer
