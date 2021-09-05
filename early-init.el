7d61c85a46f97a115cef0c3ad6b26162819388f6
Hanli Wang <hanliinter@gmail.com>
2021-09-05 09:07:38 +0200

    Summary: refine gitignore
    keep rime/ out
    enbale early-init

--- early-init.el
+++ early-init.el
@@ -1,1 +1,11 @@
 (setq package-enable-at-startup nil)
+(defvar package-quickstart t)
+
+;; Disable menubar here instead of in theme.el
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t)
+
+
+(setq native-comp-async-report-warnings-errors 'silent)
