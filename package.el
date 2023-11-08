9d8999802a89a37e81d63938e610be62dac91b48
Hanli Wang <hanliinter@gmail.com>
2023-11-08 16:13:31 +0100

    Restart to use move-text

--- package.el
+++ package.el
@@ -514,3 +514,19 @@ FACE defaults to inheriting from default and highlight."
 
 (use-package vterm
   :straight t)
+
+
+(defun indent-region-advice (&rest ignored)
+  (let ((deactivate deactivate-mark))
+    (if (region-active-p)
+        (indent-region (region-beginning) (region-end))
+      (indent-region (line-beginning-position) (line-end-position)))
+    (setq deactivate-mark deactivate)))
+
+
+(use-package move-text
+  :straight t
+  :config
+  (advice-add 'move-text-up :after 'indent-region-advice)
+  (advice-add 'move-text-down :after 'indent-region-advice))
+
